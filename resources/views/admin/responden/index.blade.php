e0b7ca4e583e79dc240f8643c45a060c4c3c3411
Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
2025-03-14 19:10:41 +0700

    [FEAT] Enhance Responden index view; improve table styling, add hover effects, and format respondent details for better readability

--- resources/views/admin/responden/index.blade.php
+++ resources/views/admin/responden/index.blade.php
@@ -2,25 +2,25 @@
 <div class="p-4 sm:ml-64">
 <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
     <div class="container mx-auto text-center pt-7">
-        <h1 class="font-bold text-[32px] pt-7 pb-7 ">Daftar Responden</h1>
-        <table class="min-w-full bg-white">
-            <thead>
-                <tr>
-                    <th class="py-2">Nama Lengkap</th>
-                    <th class="py-2">Jenis Kelamin</th>
-                    <th class="py-2">Usia</th>
-                    <th class="py-2">Pendidikan</th>
-                    <th class="py-2">Pekerjaan</th>
+        <h1 class="font-bold text-[32px] pt-7 pb-7">Daftar Responden</h1>
+        <table class="table-auto overflow-x-auto mx-auto items-center relative shadow-md sm:rounded-lg my-6 w-full max-w-full rtl:justify-left text-sm text-left text-gray-500">
+            <thead class="w-full max-w-full rtl:justify-left text-lg text-left text-gray-500 my-3">
+                <tr class="text-sm text-tertiary uppercase bg-gray-50 text-center">
+                    <th scope="col" class="px-6 py-3">Nama Lengkap</th>
+                    <th scope="col" class="px-6 py-3">Jenis Kelamin</th>
+                    <th scope="col" class="px-6 py-3">Usia</th>
+                    <th scope="col" class="px-6 py-3">Pendidikan</th>
+                    <th scope="col" class="px-6 py-3">Pekerjaan</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="text-center">
                 @foreach($respondents as $respondent)
-                <tr>
-                    <td class="border px-4 py-2">{{ $respondent->nama_lengkap }}</td>
-                    <td class="border px-4 py-2">{{ $respondent->jenis_kelamin }}</td>
-                    <td class="border px-4 py-2">{{ $respondent->usia }}</td>
-                    <td class="border px-4 py-2">{{ $respondent->pendidikan }}</td>
-                    <td class="border px-4 py-2">{{ $respondent->pekerjaan }}</td>
+                <tr class="hover:bg-gray-200 transition duration-200 cursor-pointer">
+                    <td class="py-2">{{ strtoupper($respondent->nama_lengkap) }}</td>
+                    <td class="py-2">{{ $respondent->jenis_kelamin == 'l' ? 'Laki-laki' : 'Perempuan' }}</td>
+                    <td class="py-2">{{ $respondent->usia }}</td>
+                    <td class="py-2">{{ strtoupper($respondent->pendidikan) }}</td>
+                    <td class="py-2">{{ strtoupper($respondent->pekerjaan) }}</td>
                 </tr>
                 @endforeach
             </tbody>
