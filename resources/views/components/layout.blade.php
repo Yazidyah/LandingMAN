b7a310dba8e3ddfbc03db602fcea631f0b47f430
Ibnuardhian <ibnuardhian@gmail.com>
2025-07-21 09:06:11 +0700

    nambah chatbot ke web

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,8 @@
     <link href="/css/flickity.css" rel="stylesheet" media="screen">
     <link href="logoman.png" type="image/png" rel="icon">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script> --}}
+    {{--
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script> --}}
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -19,10 +20,12 @@
     <x-navbar></x-navbar>
     <main>
         {{ $slot }}
+        <script src="https://cdn.botpress.cloud/webchat/v3.1/inject.js"></script>
+        <script src="https://files.bpcontent.cloud/2025/06/19/10/20250619105656-283UARBW.js"></script>
         <x-footer></x-footer>
         <x-javascript></x-javascript>
     </main>
-    
+
 </body>
 
 </html>
